f6319dd7a5e08d7647e7a284e0a99dca2a0c8e4a
Mehul Jain <mehuljain.it20@gmail.com>
2020-05-15 12:13:25 +0530

    Created Customer Controller Created CustomerService Creates ICustomerService

---
+++ WebApi/FoodOrdering/FoodOrdering.Web/ServiceContracts/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using FoodOrdering.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrdering.Web.ServiceContracts
+{
+    interface ICustomerService
+    {
+        List<Customers> GetAllCustomers();
+    }
+}
---
+++ WebApi/FoodOrdering/FoodOrdering.Web/Services/CustomerService.cs
@@ -0,0 +1,27 @@
+﻿using FoodOrdering.Web.Models;
+using FoodOrdering.Web.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodOrdering.Web.Services
+{
+
+    public class CustomerService : ICustomerService
+    {
+        public List<Customers> GetAllCustomers()
+        {
+            using (Online_Food_Ordering_SystemEntities db = new Online_Food_Ordering_SystemEntities())
+            {
+                List<Customers> customersList = db.Customers.ToList();
+
+                db.Configuration.LazyLoadingEnabled = false;
+
+                return customersList;
+
+            }
+
+        }
+    }
+}
